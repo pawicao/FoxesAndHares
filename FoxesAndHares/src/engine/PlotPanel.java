c9f674b46843be095984965c1aa7cec2c4ae8b42
Oskar <pawicao@gmail.com>
2019-06-03 11:46:27 +0200

    PlotPanel sharedColor

--- FoxesAndHares/src/engine/PlotPanel.java
+++ FoxesAndHares/src/engine/PlotPanel.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 public class PlotPanel extends JPanel {
 
+    private static final Color sharedColor = new Color(153,102,0);
+
     private static PlotPanel instance = null;
 
     public static PlotPanel getInstance() {
@@ -58,7 +60,16 @@ public class PlotPanel extends JPanel {
             g2.drawLine(xPadding, height - yPadding, width- xPadding, height- yPadding);
 
             updatePlot();
+
+            int foxes;
+            int hares;
             for(int i = 0; i < foxPlot.size(); ++i) {
+                foxes = foxPlot.get(i);
+                hares = harePlot.get(i);
+                if(foxes == hares) {
+                    drawPoint(i, foxes, g2, sharedColor);
+                    continue;
+                }
                 drawPoint(i, foxPlot.get(i), g2, Color.orange);
                 drawPoint(i, harePlot.get(i), g2, Color.green);
             }
